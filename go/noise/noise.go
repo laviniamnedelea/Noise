fe7044032e314a92e080b1ccc27c2fc90a078e09
Maria-lavinia Nedelea <lavinian@google.com>
2020-07-27 08:39:42 +0000

    Modified comments

--- go/noise/noise.go
+++ go/noise/noise.go
@@ -131,12 +131,12 @@ type Noise interface {
 	// given assumptions of L_0 and L_∞ sensitivities.
 	Threshold(l0Sensitivity int64, lInfSensitivity, epsilon, deltaNoise, deltaThreshold float64) float64
 
-	// ReturnConfidenceIntervalInt64 will return a pointer to the ConfidenceIntervalInt64 structure using the int64 noisedValue,
+	// ReturnConfidenceIntervalInt64 will return a ConfidenceIntervalInt64 structure using the int64 noisedValue,
 	// with the confidenceLevel given, and the l0Sensitivity, lInfSensitivity int64 and epsilon, delta float64 for the distribution.
 	ReturnConfidenceIntervalInt64(noisedValue, l0Sensitivity, lInfSensitivity int64, epsilon, delta,
 		confidenceLevel float64) (ConfidenceIntervalInt64, error)
 
-	// ReturnConfidenceIntervalFloat64 will return a pointer to the ConfidenceIntervalFloat64 structure using the float64 noisedValue,
+	// ReturnConfidenceIntervalFloat64 will return a ConfidenceIntervalFloat64 structure using the float64 noisedValue,
 	// with the confidenceLevel given, and the l0Sensitivity int64 and lInfSensitivity, epsilon, delta float64 for the distribution.
 	ReturnConfidenceIntervalFloat64(noisedValue float64, l0Sensitivity int64, lInfSensitivity, epsilon, delta,
 		confidenceLevel float64) (ConfidenceIntervalFloat64, error)
